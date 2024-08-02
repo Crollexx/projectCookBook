10b0fccd7e93a45dc3f6fd601a15a29e9b02135c
Авдеева Вероника Игоревна <avdeevaveronikaigorevna@Noutbuk-Avdeeva.local>
2024-08-02 09:38:39 +0300

    createButton

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Button() {
-    
+    const [buttonFav, setButtonFav]=useState(false)
+    const butFunction =()=>{
+       if (setButtonFav===true){
+        <>
+        <button />
+        </>
+       }
+    }
 
   return (
-    <></>
+    <>
+    <button onClick={butFunction}></button>
+    </>
   )
 }
 
